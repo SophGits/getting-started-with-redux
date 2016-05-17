f2cd2e3ad044abfcd73fb00277ad32da4d61d91d
SophGits <sophie.jiffy@gmail.com>
2016-05-17 13:46:04 +0100

    Initial commit: create file structure & check setup works.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Redux Practice
+Following Dan Abramov's [Getting Started with Redux](https://egghead.io/lessons/javascript-redux-writing-a-todo-list-reducer-adding-a-todo)
+
+## To start
+`npm run build`
+
+## NB
+For this configuration, Babelify cannot be later than v6.2.0
---
+++ src/script.js
@@ -0,0 +1,31 @@
+import deepFreeze from 'deep-freeze';
+import expect from 'expect';
+
+const todos = (state = [], action) => {
+
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+testAddTodo();
+console.log('All tests have passed');
